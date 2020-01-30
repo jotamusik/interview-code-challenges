8ea43c1dc1edb019202a65db145a773e0fb7f9fd
Jorge Aguiar Martin <jotamusik@gmail.com>
2020-01-30 13:05:16 +0000

    red - Robot.movements

--- node/martian-robots/test/example.test.js
+++ node/martian-robots/test/example.test.js
@@ -1,4 +1,4 @@
-import { Mars, Position } from "../src/example";
+import { Mars, Orientations, Position, Robot } from "../src/example";
 
 
 describe('Mars Planet', () => {
@@ -49,5 +49,38 @@ describe('Robot', () => {
     expect(robot.getPosition().getX()).toBe(x);
     expect(robot.getPosition().getY()).toBe(y);
     expect(robot.getPosition().getOrientation()).toBe(orientation);
+  });
+  test('Should move forward', () => {
+    let x = 2;
+    let y = 3;
+    let orientation = 'N';
+    let position = new Position(x, y, orientation);
+    let robot = new Robot(position);
+    robot.move('F');
+    expect(robot.getPosition().getX()).toBe(x);
+    expect(robot.getPosition().getY()).toBe( (y + 1) );
+    expect(robot.getPosition().getOrientation()).toBe(orientation);
+  });
+  test('Should move left', () => {
+    let x = 2;
+    let y = 3;
+    let orientation = 'N';
+    let position = new Position(x, y, orientation);
+    let robot = new Robot(position);
+    robot.move('L');
+    expect(robot.getPosition().getX()).toBe(x);
+    expect(robot.getPosition().getY()).toBe(y);
+    expect(robot.getPosition().getOrientation()).toBe('W');
+  });
+  test('Should move right', () => {
+    let x = 2;
+    let y = 3;
+    let orientation = 'N';
+    let position = new Position(x, y, orientation);
+    let robot = new Robot(position);
+    robot.move('R');
+    expect(robot.getPosition().getX()).toBe(x);
+    expect(robot.getPosition().getY()).toBe(y);
+    expect(robot.getPosition().getOrientation()).toBe('E');
   });
 });
